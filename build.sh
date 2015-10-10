8b8b55ad3ea7fb00b1ccff9f93728763eb7e14c8
DC07 <deveshnegi.3105.dn@gmail.com>
2015-10-10 05:49:21 -0400

    spirit: Changes in build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+export KBUILD_BUILD_USER="devesh"
+export KBUILD_BUILD_HOST="work2"
+export ARCH=arm64
+export SUBARCH=arm64
+export CROSS_COMPILE=/home/devesh/spirit/tc/aarch64/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
+make cyanogenmod_tomato-64_defconfig
+make -j64
+
